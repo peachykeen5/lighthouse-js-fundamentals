ea6a14fcfa1ba2020ddd4b282436fc24aabb56a1
peachykeen5 <lexi.keen@gmail.com>
2018-03-18 15:24:23 +0000

    Adds looping exercises

---
+++ whichschool.js
@@ -0,0 +1,6 @@
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
