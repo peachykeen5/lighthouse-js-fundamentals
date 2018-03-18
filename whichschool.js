5e897a816473a235f4a4b2b5da3843313b9685a9
peachykeen5 <lexi.keen@gmail.com>
2018-03-18 15:58:34 +0000

    Adds ingredients exercise

--- whichschool.js
+++
@@ -1,6 +0,0 @@
-console.log("I am 35. Which school should I go to?");
-console.log(whichSchool(35));
-console.log("I am 8. Which school should I go to?");
-console.log(whichSchool(8));
-console.log("I am 14. Which school should I go to?");
-console.log(whichSchool(14));
